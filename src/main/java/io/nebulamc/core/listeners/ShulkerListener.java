7f7d4f039335eefc564ff995d51f8f8ed372b1df
Ludwig <km0616k@gmail.com>
2021-09-10 19:35:04 +0900

    Update ShulkerListener

--- src/main/java/io/nebulamc/core/listeners/ShulkerListener.java
+++ src/main/java/io/nebulamc/core/listeners/ShulkerListener.java
@@ -1,5 +1,6 @@
 package io.nebulamc.core.listeners;
 
+import com.google.common.collect.ImmutableSet;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -10,19 +11,42 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Set;
 
 import static io.nebulamc.core.Core.PREFIX;
 
 public class ShulkerListener implements Listener {
 
+    private static Set<Material> bannedFromShulker = ImmutableSet.of(
+            Material.GOLDEN_APPLE,
+            Material.ENCHANTED_GOLDEN_APPLE,
+            Material.EXPERIENCE_BOTTLE,
+            Material.POTION,
+            Material.SPLASH_POTION,
+            Material.LINGERING_POTION,
+            Material.GOLD_NUGGET,
+            Material.GOLD_INGOT,
+            Material.GOLD_ORE,
+            Material.DEEPSLATE_GOLD_ORE,
+            Material.GOLD_BLOCK,
+            Material.RAW_GOLD,
+            Material.RAW_GOLD_BLOCK);
+
     @EventHandler
     public void onInventoryMove(InventoryClickEvent event) {
         if (event.getInventory().getType() == InventoryType.SHULKER_BOX) {
-            if(event.getClick().isKeyboardClick()){
+            if (event.getClick().isKeyboardClick()) {
                 event.setCancelled(true);
                 Bukkit.getPlayer(event.getWhoClicked().getUniqueId()).sendMessage(PREFIX + "Hotkey movement is disabled for shulkers.");
             }
-            if (event.getCurrentItem().getType() == Material.GOLDEN_APPLE || event.getCurrentItem().getType() == Material.ENCHANTED_GOLDEN_APPLE || event.getCurrentItem().getType() == Material.EXPERIENCE_BOTTLE || event.getCurrentItem().getType() == Material.POTION || event.getCurrentItem().getType() == Material.SPLASH_POTION || event.getCurrentItem().getType() == Material.LINGERING_POTION || event.getCurrentItem().getType() == Material.GOLD_NUGGET || event.getCurrentItem().getType() == Material.GOLD_INGOT || event.getCurrentItem().getType() == Material.GOLD_ORE || event.getCurrentItem().getType() == Material.DEEPSLATE_GOLD_ORE || event.getCurrentItem().getType() == Material.GOLD_BLOCK || event.getCurrentItem().getType() == Material.RAW_GOLD || event.getCurrentItem().getType() == Material.RAW_GOLD_BLOCK) {
+
+            ItemStack currentItem = event.getCurrentItem();
+            if (currentItem == null)
+                return;
+
+            if (bannedFromShulker.contains(currentItem.getType())) {
                 event.setCancelled(true);
                 Bukkit.getPlayer(event.getWhoClicked().getUniqueId()).sendMessage(PREFIX + "You can not move that item into shulkers!");
             }
@@ -30,16 +54,14 @@ public class ShulkerListener implements Listener {
     }
 
 
-
     @EventHandler
     public void onHopperPlace(BlockPlaceEvent event) {
-        if(event.getBlockPlaced().getType() == Material.HOPPER || event.getBlockPlaced().getType() == Material.HOPPER_MINECART){
-            if(event.getBlockAgainst().getType() == Material.SHULKER_BOX || event.getBlockAgainst().getType() == Material.BLACK_SHULKER_BOX || event.getBlockAgainst().getType() == Material.WHITE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.BLUE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.CYAN_SHULKER_BOX || event.getBlockAgainst().getType() == Material.BROWN_SHULKER_BOX || event.getBlockAgainst().getType() == Material.GRAY_SHULKER_BOX || event.getBlockAgainst().getType() == Material.GREEN_SHULKER_BOX || event.getBlockAgainst().getType() == Material.LIGHT_BLUE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.LIGHT_GRAY_SHULKER_BOX || event.getBlockAgainst().getType() == Material.LIME_SHULKER_BOX || event.getBlockAgainst().getType() == Material.MAGENTA_SHULKER_BOX || event.getBlockAgainst().getType() == Material.ORANGE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.PINK_SHULKER_BOX || event.getBlockAgainst().getType() == Material.PURPLE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.RED_SHULKER_BOX || event.getBlockAgainst().getType() == Material.YELLOW_SHULKER_BOX){
+        if (event.getBlockPlaced().getType() == Material.HOPPER || event.getBlockPlaced().getType() == Material.HOPPER_MINECART) {
+            if (event.getBlockAgainst().getType().toString().endsWith("SHULKER_BOX")) {
                 event.setCancelled(true);
                 event.getPlayer().sendMessage(PREFIX + "You can not place hoppers going into shulkers!");
             }
-        }
-        else if(event.getBlockPlaced().getType() == Material.SHULKER_BOX || event.getBlockAgainst().getType() == Material.BLACK_SHULKER_BOX || event.getBlockAgainst().getType() == Material.WHITE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.BLUE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.CYAN_SHULKER_BOX || event.getBlockAgainst().getType() == Material.BROWN_SHULKER_BOX || event.getBlockAgainst().getType() == Material.GRAY_SHULKER_BOX || event.getBlockAgainst().getType() == Material.GREEN_SHULKER_BOX || event.getBlockAgainst().getType() == Material.LIGHT_BLUE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.LIGHT_GRAY_SHULKER_BOX || event.getBlockAgainst().getType() == Material.LIME_SHULKER_BOX || event.getBlockAgainst().getType() == Material.MAGENTA_SHULKER_BOX || event.getBlockAgainst().getType() == Material.ORANGE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.PINK_SHULKER_BOX || event.getBlockAgainst().getType() == Material.PURPLE_SHULKER_BOX || event.getBlockAgainst().getType() == Material.RED_SHULKER_BOX || event.getBlockAgainst().getType() == Material.YELLOW_SHULKER_BOX){
+        } else if (event.getBlockPlaced().getType().toString().endsWith("SHULKER_BOX")) {
             Location loc = event.getBlockPlaced().getLocation();
             Block b1 = loc.getBlock().getRelative(BlockFace.NORTH);
             Block b2 = loc.getBlock().getRelative(BlockFace.EAST);
@@ -48,7 +70,7 @@ public class ShulkerListener implements Listener {
             Block b5 = loc.getBlock().getRelative(BlockFace.UP);
             Block b6 = loc.getBlock().getRelative(BlockFace.DOWN);
 
-            if(b1.getType() == Material.HOPPER || b2.getType() == Material.HOPPER || b3.getType() == Material.HOPPER || b4.getType() == Material.HOPPER || b5.getType() == Material.HOPPER || b6.getType() == Material.HOPPER || b1.getType() == Material.HOPPER_MINECART || b2.getType() == Material.HOPPER_MINECART || b3.getType() == Material.HOPPER_MINECART || b4.getType() == Material.HOPPER_MINECART || b5.getType() == Material.HOPPER_MINECART || b6.getType() == Material.HOPPER_MINECART) {
+            if (b1.getType() == Material.HOPPER || b2.getType() == Material.HOPPER || b3.getType() == Material.HOPPER || b4.getType() == Material.HOPPER || b5.getType() == Material.HOPPER || b6.getType() == Material.HOPPER || b1.getType() == Material.HOPPER_MINECART || b2.getType() == Material.HOPPER_MINECART || b3.getType() == Material.HOPPER_MINECART || b4.getType() == Material.HOPPER_MINECART || b5.getType() == Material.HOPPER_MINECART || b6.getType() == Material.HOPPER_MINECART) {
                 event.getPlayer().sendMessage(PREFIX + "You can not place shulkers by hoppers!");
                 event.setCancelled(true);
             }
