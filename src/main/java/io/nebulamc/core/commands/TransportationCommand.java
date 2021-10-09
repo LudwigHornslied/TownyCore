85b9ff527670a8f7dc35274b95b6c76b0ab0229a
Ludwig <km0616k@gmail.com>
2021-10-10 02:15:21 +0900

    Add /boat, /cart command

---
+++ src/main/java/io/nebulamc/core/commands/TransportationCommand.java
@@ -0,0 +1,48 @@
+package io.nebulamc.core.commands;
+
+import io.nebulamc.core.Core;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Vehicle;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+
+public class TransportationCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            Bukkit.getLogger().info("[TownyCore]: You need to be a player to execute this command.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        Vehicle vehicle;
+        if(cmd.getLabel().equalsIgnoreCase("boat")) {
+            if(!player.getLocation().add(0.0, -1.0, 0.0).getBlock().getType().toString().endsWith("ICE")) {
+                sender.sendMessage(ChatColor.RED + "You must be on an ice block to run this command.");
+                return true;
+            }
+            vehicle = (Boat) player.getWorld().spawnEntity(player.getLocation(), EntityType.BOAT);
+        } else {
+            if(!player.getLocation().getBlock().getType().toString().endsWith("RAIL")) {
+                sender.sendMessage(ChatColor.RED + "You must be on railroad to run this command.");
+                return true;
+            }
+            vehicle = (Minecart) player.getWorld().spawnEntity(player.getLocation(), EntityType.MINECART);
+        }
+        vehicle.addPassenger(player);
+        vehicle.getPersistentDataContainer().set(new NamespacedKey(Core.getInstance(), "CustomVehicle"), PersistentDataType.BYTE, (byte) 0);
+
+        return true;
+    }
+}
