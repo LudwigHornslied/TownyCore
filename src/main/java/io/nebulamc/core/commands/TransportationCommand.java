35d8284bb80b8a362c42d146ebc38f6eb19e029f
0xBit <73406588+0xBit-MC@users.noreply.github.com>
2021-10-09 23:39:43 -0700

    Add plugin prefix & remove ChatColor

--- src/main/java/io/nebulamc/core/commands/TransportationCommand.java
+++ src/main/java/io/nebulamc/core/commands/TransportationCommand.java
@@ -2,7 +2,6 @@ package io.nebulamc.core.commands;
 
 import io.nebulamc.core.Core;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
@@ -20,7 +19,7 @@ public class TransportationCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
         if (!(sender instanceof Player)) {
-            Bukkit.getLogger().info("[TownyCore]: You need to be a player to execute this command.");
+            Bukkit.getLogger().info(Core.PREFIX + "You need to be a player to execute this command.");
             return true;
         }
 
@@ -29,13 +28,13 @@ public class TransportationCommand implements CommandExecutor {
         Vehicle vehicle;
         if(cmd.getLabel().equalsIgnoreCase("boat")) {
             if(!player.getLocation().add(0.0, -1.0, 0.0).getBlock().getType().toString().endsWith("ICE")) {
-                sender.sendMessage(ChatColor.RED + "You must be on an ice block to run this command.");
+                sender.sendMessage(Core.PREFIX + "§cYou must be on an ice block to run this command.");
                 return true;
             }
             vehicle = (Boat) player.getWorld().spawnEntity(player.getLocation(), EntityType.BOAT);
         } else {
             if(!player.getLocation().getBlock().getType().toString().endsWith("RAIL")) {
-                sender.sendMessage(ChatColor.RED + "You must be on railroad to run this command.");
+                sender.sendMessage(Core.PREFIX + "§cYou must be on railroad to run this command.");
                 return true;
             }
             vehicle = (Minecart) player.getWorld().spawnEntity(player.getLocation(), EntityType.MINECART);
